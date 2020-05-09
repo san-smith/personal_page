6d77c543e676b4c503f8b56699bfde81d588e41d
san-smith <san-smith@mail.ru>
2020-05-09 15:22:38 +0700

    Add "fork me" button

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:personal_page/internal/app_config.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'widgets/main_page.dart';
 
@@ -18,11 +19,42 @@ class _HomeState extends State<Home> {
   }
 
   Widget _getBody() {
-    return PageView(
-      scrollDirection: Axis.vertical,
+    return Stack(
       children: <Widget>[
-        MainPage(),
+        PageView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            MainPage(),
+          ],
+        ),
+        Positioned(
+          right: 20,
+          top: 20,
+          child: _getForkButton(),
+        ),
       ],
     );
+  }
+
+  Widget _getForkButton() {
+    return OutlineButton(
+      textColor: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text('Fork me on GitHub!'),
+          SizedBox(width: 10),
+          FaIcon(FontAwesomeIcons.github),
+        ],
+      ),
+      onPressed: _onGitHubTap,
+    );
+  }
+
+  void _onGitHubTap() async {
+    const url = 'https://github.com/san-smith/personal_page';
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
   }
 }
